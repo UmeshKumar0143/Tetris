1e53bd635f1ddb615d90de6f85307f2a3b433d54
UmeshKumar0143 <umeshkumar153654@gmail.com>
2025-07-12 16:15:02 +0530

    game environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <raylib.h>
 
 int main(){
-    
+    Color darkblue  = {44, 44, 127, 255}; 
+    InitWindow(300, 600, "tetrix"); 
+    SetTargetFPS(60); 
+    while(!WindowShouldClose()){
+        BeginDrawing(); 
+        ClearBackground(darkblue); 
+        EndDrawing(); 
+    }
+
+    CloseWindow(); 
 }
